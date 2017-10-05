b0bb2aaa28dd40dbdca49199cc765c1af3ce6276
Jonathan Longden <longden.jonathanedgar@gmail.com>
2017-10-04 21:36:29 -0600

    storing other p5 tasks

--- piper.js
+++ piper.js
@@ -1,14 +1,16 @@
+/*jshint esversion: 6 */
+const BORDER = 10;
 const CANVAS_WIDTH = 1000;
 const CANVAS_HEIGHT = 800;
-const BELOW_TITLE = 200;
-const TRI_WIDTH = 300;
+const BELOW_TITLE = CANVAS_WIDTH - CANVAS_HEIGHT - BORDER;
+const TRI_WIDTH = BELOW_TITLE;
 //aways Centered
 const X_1 = (CANVAS_WIDTH / 2); //middle
 const X_2 = ((CANVAS_WIDTH / 2) - TRI_WIDTH); //left
 const X_3 = ((CANVAS_WIDTH / 2) + TRI_WIDTH); //right
 
 const Y_1 = BELOW_TITLE; //top
-const Y_2 = TRI_WIDTH + BELOW_TITLE; //bottom
+const Y_2 = CANVAS_HEIGHT - BORDER; //bottom
 const Y_3 = Y_2; //bottom
 
 
@@ -35,4 +37,33 @@ function body() {
 
 function title() {
 
+}
+
+function mouseClicked() {
+    var imgIndex = findindex(mouseX, mouseY);
+    console.log(imgIndex);
+}
+
+function findindex(x, y) {
+    var clickedx = x; //(i % ROWS) * IMAGE_WIDTH;
+    var clickedy = y; //Math.floor(i / ROWS) * IMAGE_HEIGHT;
+
+    if (insideTriangle(clickedx, clickedy)) {
+        return 1;
+    }
+
+    return -1;
+}
+
+function insideTriangle(clickedx, clickedy) {
+    var triangleArea = tArea(X_1, X_2, X_3, Y_1, Y_2, Y_3);
+    var A = tArea(clickedx, X_2, X_3, clickedy, Y_2, Y_3);
+    var B = tArea(X_1, clickedx, X_3, Y_1, clickedy, Y_3);
+    var C = tArea(X_1, X_2, clickedx, Y_1, Y_2, clickedy);
+    return (triangleArea === A + B + C);
+}
+
+function tArea(x1, x2, x3, y1, y2, y3) {
+    area = Math.abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2);
+    return area;
 }
